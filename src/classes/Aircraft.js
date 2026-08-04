600c19bcd5c0f33b93407a19e527ff80978442bb
Zp1021 <90364004+Zp1021@users.noreply.github.com>
2026-08-04 15:32:33 -0400

    Implemented Aircraft class and simulated basic movement

---
+++ src/classes/Aircraft.js
@@ -0,0 +1,28 @@
+export default class Aircraft{
+
+    // Information for aircraft
+    constructor(x, y, vx, vy) {
+        // Starting positions
+        this.x = x;
+        this.y = y;
+
+        // Speed along each axis
+        this.vx = vx;
+        this.vy = vy;
+    }
+
+    // Updated positional info based on change over time
+    update(dt){
+        this.x += this.vx * dt;
+        this.y += this.vy * dt;
+    }
+
+    // Return position information
+    getPosition(){
+        return {
+            x: this.x,
+            y: this.y
+        };
+    }
+
+}
---
+++ src/main.js
@@ -0,0 +1,40 @@
+import Aircraft from "./classes/Aircraft.js";
+
+const aircraft = new Aircraft(
+    // Test positions 
+    100,
+    100,
+    50,
+    20
+)
+
+// Constant change in time by 1 second
+const dt = 1;
+
+// Counting update intervals
+let count = 0;
+
+// Tracking limit
+const maxRuns = 5;
+
+// Interval function called every 1000 milliseconds
+const intervalId = setInterval(() => {
+  
+  count++;  
+  // Update aircraft position every 1 second
+  aircraft.update(dt);
+
+  // Log update count and aircraft position
+  console.log(`Updated ${count} time(s)`);
+  console.log(aircraft.getPosition());
+
+  // If the update count is greater than the tracking limit
+  if (count >= maxRuns) {
+
+    // Stops the interval
+    clearInterval(intervalId); 
+    
+    //Log to console that tracking has stopped
+    console.log("Tracking stopped.");
+  }
+}, 1000);
